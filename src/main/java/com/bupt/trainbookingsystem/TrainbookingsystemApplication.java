8ee15eb3f4f6e5de1d1251fb97a3d51eed7b021e
yangweidong <1648197192@qq.com>
2020-05-11 21:08:01 +0800

    第一次提交

---
+++ src/main/java/com/bupt/trainbookingsystem/TrainbookingsystemApplication.java
@@ -0,0 +1,13 @@
+package com.bupt.trainbookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainbookingsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainbookingsystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bupt/trainbookingsystem/TrainbookingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bupt.trainbookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainbookingsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
