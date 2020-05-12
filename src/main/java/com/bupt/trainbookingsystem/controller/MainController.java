b29946cf91262eb8d2d01ed821f7ccf6d75fb33e
yangweidong <1648197192@qq.com>
2020-05-12 17:35:21 +0800

    更新Controller

--- src/main/java/com/bupt/trainbookingsystem/controller/MainController.java
+++ src/main/java/com/bupt/trainbookingsystem/controller/MainController.java
@@ -1,4 +1,15 @@
 package com.bupt.trainbookingsystem.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class MainController {
+
+    @RequestMapping("/index")
+    public String getIndex(){
+        return "template";
+    }
+
 }
