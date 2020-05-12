d33d911498b0d7c073274c9d4e33068ea7250931
yangweidong <1648197192@qq.com>
2020-05-12 20:19:19 +0800

    提交系统url

--- src/main/java/com/bupt/trainbookingsystem/controller/MainController.java
+++ src/main/java/com/bupt/trainbookingsystem/controller/MainController.java
@@ -9,14 +9,43 @@ import org.springframework.web.bind.annotation.RequestParam;
  * 开发者：杨韦岽
  * 内容：系统的项目导航
  * index 首页
+ * search 查询车票
  *
  */
 @Controller
 public class MainController {
 
+    /**
+     * 首页url
+     * @return
+     */
     @RequestMapping("/index")
     public String getIndex(){
         return "index";
+    }
+
+    /**
+     * 查询页面
+     * @return
+     */
+    @RequestMapping("/search")
+    public String getSearch(){
+        return "search";
+    }
+
+
+    /**
+     * 买票页面
+     * @return
+     */
+    @RequestMapping("/buyTicket")
+    public String getBuyTicket(){
+        return "buyticket";
+    }
+
+    @RequestMapping("/pay")
+    public String getPay(){
+        return "pay";
     }
 
 }
