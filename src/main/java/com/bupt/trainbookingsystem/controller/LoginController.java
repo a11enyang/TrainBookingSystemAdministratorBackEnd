df76a469eafe23e17c075777f9017f577bbb8845
1833982205 <1833982205@qq.com>
2020-05-13 14:47:55 +0800

    售票端页面修改

--- src/main/java/com/bupt/trainbookingsystem/controller/LoginController.java
+++ src/main/java/com/bupt/trainbookingsystem/controller/LoginController.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import javax.servlet.http.HttpSession;
 
 @Controller
