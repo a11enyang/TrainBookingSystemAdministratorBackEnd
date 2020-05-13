706dae2d4884ae1645161f8cb5c28843d38ebeef
1291869157 <1291869157@qq.com>
2020-05-13 17:02:32 +0800

    pcentermodify

--- src/main/java/com/bupt/trainbookingsystem/controller/MainController.java
+++ src/main/java/com/bupt/trainbookingsystem/controller/MainController.java
@@ -1,9 +1,13 @@
 package com.bupt.trainbookingsystem.controller;
 
+import com.bupt.trainbookingsystem.entity.OrdinaryUserEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * 开发者：杨韦岽
@@ -28,8 +32,12 @@ public class MainController {
      * 查询页面
      * @return
      */
-    @RequestMapping("/search")
-    public String getSearch(){
+    @GetMapping("/search")
+    public String search(HttpSession session, Model model){
+
+        OrdinaryUserEntity user=(OrdinaryUserEntity) session.getAttribute("user");
+        if(user!=null)
+            model.addAttribute("names",user.getName());
         return "search";
     }
 
@@ -57,7 +65,14 @@ public class MainController {
      * @return
      */
     @RequestMapping("/pcenter")
-    public String getPersonalCenter() {
+    public String getPersonalCenter(HttpSession session,Model model) {
+        OrdinaryUserEntity user=(OrdinaryUserEntity) session.getAttribute("user");
+        if(user!=null) {
+            model.addAttribute("names", user.getName());
+            model.addAttribute("types", user.getType());
+            model.addAttribute("personIds", user.getPersonId());
+            model.addAttribute("phonenums", user.getPhonenum());
+        }
         return "personalcenter";
     }
 }
