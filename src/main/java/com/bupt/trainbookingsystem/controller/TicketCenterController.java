e3a9e7a8c9723e75118066a42ec098d69879c762
XYT2000 <1026310947@qq.com>
2020-05-25 21:37:13 +0800

    删除冗余

--- src/main/java/com/bupt/trainbookingsystem/controller/TicketCenterController.java
+++ src/main/java/com/bupt/trainbookingsystem/controller/TicketCenterController.java
@@ -60,50 +60,5 @@ public class TicketCenterController {
     public String backLogin(){
         return "redirect:/ticketLogin";
     }
-    /**
-     * post请求（用于请求json格式的参数）
-     *
-     * @param url  地址
-     * @param params json格式的参数
-     * @return
-     */
-    public static String doPost(String url, String params) throws Exception {
 
-        CloseableHttpClient httpclient = HttpClients.createDefault();
-        HttpPost httpPost = new HttpPost( url );// 创建httpPost
-        httpPost.setHeader( "Accept", "application/json" );
-        httpPost.setHeader( "Content-Type", "application/json" );
-        String charSet = "UTF-8";
-        StringEntity entity = new StringEntity( params, charSet );
-        httpPost.setEntity( entity );
-        CloseableHttpResponse response = null;
-
-        try {
-
-            response = httpclient.execute(httpPost);
-            StatusLine status = response.getStatusLine();
-            int state = status.getStatusCode();
-            if (state == HttpStatus.SC_OK) {
-                HttpEntity responseEntity = response.getEntity();
-                String jsonString = EntityUtils.toString( responseEntity );
-                return jsonString;
-            } else {
-
-            }
-        } finally {
-            if (response != null) {
-                try {
-                    response.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            try {
-                httpclient.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        return null;
-    }
 }
