4ad20aca5ef051dab3b05e2dd89e50144ffb2e33
1291869157 <1291869157@qq.com>
2020-06-02 22:12:00 +0800

    interceptor测试

--- src/main/java/com/bupt/trainbookingsystem/intersepter/Intersepterconfig.java
+++ src/main/java/com/bupt/trainbookingsystem/intersepter/Intersepterconfig.java
@@ -4,14 +4,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/*
 @Configuration
 public class Intersepterconfig implements WebMvcConfigurer {
-/*    @Override
+    @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        String[] excludes=new String[]{"/api/**","/static/**"};
         registry.addInterceptor(new Intersepter())
                 .addPathPatterns("/**")
                 .excludePathPatterns("/login")
                 .excludePathPatterns("/")
-                .excludePathPatterns("/search");
-    }*/
-}
+                .excludePathPatterns("/search")
+                .excludePathPatterns("/pay/{id}")
+                .excludePathPatterns("/api/**")
+                .excludePathPatterns("/static/**");
+
+    }
+}*/
