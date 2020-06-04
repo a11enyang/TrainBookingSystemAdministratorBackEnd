ee023053cecdf49414b291dcc3837dc7750a13da
1833982205 <1833982205@qq.com>
2020-06-04 11:38:51 +0800

    intersepter拦截器

--- src/main/java/com/bupt/trainbookingsystem/controller/ManageCenterController.java
+++ src/main/java/com/bupt/trainbookingsystem/controller/ManageCenterController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
 import java.util.Map;
 
 /**
@@ -28,10 +30,13 @@ public class ManageCenterController {
     //检查管理员登录
     @RequestMapping("manager/login")
     public String checkManager(@RequestParam("name") String name,
-                               @RequestParam("pw") String pw, Map<String,Object> map){
+                               @RequestParam("pw") String pw,
+                               Map<String,Object> map,
+                               HttpSession session){
         Administrator = administratorService.findAdministratorEntityByNameAndPassword(name,pw);
         if(Administrator!=null ) {
             System.out.println("用户"+name+"登录");
+            session.setAttribute("Administrator",Administrator);
             return "managercenter";
         }
         else{
